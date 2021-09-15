0d4928094d5e8ca6d1d8996f439b8bfc23430c2e
suryansh <suryansh.lucifer@gmail.com>
2021-09-15 17:57:04 -0400

    code edited

--- spaceship.py
+++ spaceship.py
@@ -14,10 +14,10 @@ print("How many companions do you have?")
 companions = 0
 
 # Add your input code here
-
+print(2+3)
 print("Where do you want to go?")
 planet = ""
 
 # Add your code here
-
+print("Hello World")
 print("The Spaceship is taking off with {} people to planet {} ").format(companions+1, planet)
